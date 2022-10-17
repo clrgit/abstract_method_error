45829f838910082310fb9aa04cbddd058c5617a5
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2022-10-18 00:22:22 +0200

    Initial setup

---
+++ lib/abstract_method.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "abstract_method/version"
+
+module AbstractMethod
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/abstract_method_spec.rb
@@ -0,0 +1,7 @@
+describe AbstractMethod do
+  it 'has a version number' do
+    expect(AbstractMethod::VERSION).not_to be_nil
+  end
+
+  it 'does something useful'
+end
